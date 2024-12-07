c9e857f5949abd37d6fdfbdbfb55305140dac505
Jessica Amy <jessicaannetteamy@gmail.com>
2024-12-07 13:08:45 +1030

    Finished class

--- ,main.py
+++ ,main.py
@@ -1,7 +1,10 @@
 from file_operations import load_matches, save_matches
+from match_operations import display_matches
 
 spam = load_matches("matches.json")
 
 spam[0]['Score A'] = 42
 
 save_matches('debug.json', spam)
+
+display_matches(spam)
